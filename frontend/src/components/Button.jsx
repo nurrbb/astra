fc127c517c8655b17be138ca3f8ebef1c2fafd6a
nurrbb <enurb320@gmail.com>
2025-12-16 17:38:10 +0300

    feat(ui): Add reusable Button component with styles

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,17 @@
+import './Button.css'
+
+const Button = ({ children, onClick, type = 'button', variant = 'primary', disabled = false }) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={`btn btn-${variant}`}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
+
